f65e35303f7fe5e4ba3275f058e113b913cd280b
O V V S Ganapathi <othojuganapathi123@gmail.com>
2026-02-21 12:04:20 +0530

    updated the oauthcallback page

--- frontend/src/features/auth/pages/OAuthCallbackPage.tsx
+++ frontend/src/features/auth/pages/OAuthCallbackPage.tsx
@@ -1,36 +1,40 @@
 
 import { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from '../../../components/LoadingSpinner';
 import { fetchUser } from '../authSlice';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { useAppDispatch } from '../../../app/hooks';
 
 export default function OAuthCallback() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const { loading } = useAppSelector((state) => state.auth);
-  
+
   useEffect(() => {
-
-    const hash = window.location.hash.substring(1); // remove '#'
+    const hash = window.location.hash.substring(1);
     const params = new URLSearchParams(hash);
-    const token = params.get('token');
+    const token = params.get("token");
 
-    console.log("token in oauthCallback: ",token)
-
-    if (token) {
-      localStorage.setItem("token", token);
-      dispatch(fetchUser());
-    } else {
-      navigate('/login');
+    if (!token) {
+      navigate("/login");
+      return;
     }
-  }, []);
 
-  if(loading) {
-    return <div className='flex h-screen w-screen items-center justify-center'>
-      <LoadingSpinner text="loading user..."/>
+    localStorage.setItem("token", token);
+
+    dispatch(fetchUser())
+      .unwrap()
+      .then(() => {
+        navigate("/");
+      })
+      .catch(() => {
+        navigate("/login");
+      });
+
+  }, [dispatch, navigate]);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <LoadingSpinner text="Loading user..." />
     </div>
-  }
-
-  return <Navigate to="/" replace />
+  );
 }
